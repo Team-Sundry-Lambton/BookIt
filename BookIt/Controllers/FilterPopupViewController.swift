adebb4ddd8a81f2061c1920735531fcd2e381b71
Alice <c0874203@mylambton.ca>
2023-03-19 19:47:20 -0400

    Added Filter popup custom

---
+++ BookIt/Controllers/FilterPopupViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FilterPopupViewController.swift
+//  BookIt
+//
+//  Created by Alice’z Poy on 2023-03-19.
+//
+
+import UIKit
+
+class FilterPopupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
